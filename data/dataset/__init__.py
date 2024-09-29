ba28ab47104ac7a14c68009d140cd89c433f2ef3
liuwj2000 <44312395+liuwj2000@users.noreply.github.com>
2024-09-29 13:07:17 +0800

    Delete data directory

--- data/dataset/__init__.py
+++
@@ -1,9 +0,0 @@
-from libcity.data.dataset.abstract_dataset import AbstractDataset
-from libcity.data.dataset.trajectory_dataset import TrajectoryDataset
-
-
-__all__ = [
-    "AbstractDataset",
-    "TrajectoryDataset",
- 
-]
